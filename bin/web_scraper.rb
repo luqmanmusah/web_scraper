3fa79b011c565128f617fcd2f9a250f9d6c83223
luqmanmusah <musahluqman@gmail.com>
2021-03-10 18:22:54 +0000

    Add function to itirate

--- bin/web_scraper.rb
+++ bin/web_scraper.rb
@@ -6,8 +6,15 @@ def scraper
     url = "https://meqasa.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    featured_house = parsed_page.css('div.one-featured-prop') #121 houses
+    first_house = parsed_page.css('div.one-featured-prop') #121 houses
+    first_house.each do |houses|
+        house = {
+            Status: first_house.css('label').text,
+            Information: first_house.css('p').text,
+        }
     byebug
+    end
+    
 end
 
 scraper
