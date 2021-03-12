54b0b550c589cd0f4080da08cf4091820d1ae736
luqmanmusah <musahluqman@gmail.com>
2021-03-12 14:37:13 +0000

    Add an empty array to save scrapped data

--- bin/web_scraper.rb
+++ bin/web_scraper.rb
@@ -6,15 +6,16 @@ def scraper
     url = "https://meqasa.com/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    first_house = parsed_page.css('div.one-featured-prop') #121 houses
-    first_house.each do |houses|
+    houses = Array.new
+    first_house = parsed_page.css('div.one-featured-prop').first #121 houses
+    first_house.each do |homes|
         house = {
             Status: first_house.css('label').text,
             Information: first_house.css('p').text,
         }
-    byebug
+    houses << house
     end
-    
+    byebug
 end
 
 scraper
