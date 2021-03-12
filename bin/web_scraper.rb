be38b31e8a77b377ace9650869f94512e6274395
luqmanmusah <musahluqman@gmail.com>
2021-03-12 16:04:02 +0000

    Fix linters errors

--- bin/web_scraper.rb
+++ bin/web_scraper.rb
@@ -3,19 +3,18 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-    url = "https://meqasa.com/"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    houses = Array.new
-    first_house = parsed_page.css('div.one-featured-prop').first #121 houses
-    first_house.each do |homes|
-        house = {
-            Status: first_house.css('label').text,
-            Information: first_house.css('p').text,
-        }
+  url = 'https://meqasa.com/'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  houses = []
+  first_house = parsed_page.css('div.one-featured-prop') # 121 houses
+  first_house.each do |_homes|
+    house = {
+      Status: first_house.css('label').text,
+      Information: first_house.css('p').text
+    }
     houses << house
-    end
-    byebug
+  end
+  #   byebug
 end
-
 scraper
