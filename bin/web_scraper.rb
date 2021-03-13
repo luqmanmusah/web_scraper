2cbb99fb5e7d7b6a182f72d03cac57b77ec7fde7
luqmanmusah <musahluqman@gmail.com>
2021-03-13 10:19:41 +0000

    Extract only the data needed

--- bin/web_scraper.rb
+++ bin/web_scraper.rb
@@ -2,19 +2,32 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-def scraper
-  url = 'https://meqasa.com/'
-  unparsed_page = HTTParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
-  houses = []
-  first_house = parsed_page.css('div.one-featured-prop') # 121 houses
-  first_house.each do |_homes|
-    house = {
-      Status: first_house.css('label').text,
-      Information: first_house.css('p').text
-    }
-    houses << house
-  end
-  #   byebug
+def scraper 
+  url = 'https://meqasa.com/' 
+  unparsed_page = HTTParty.get(url) 
+  parsed_page = Nokogiri::HTML(unparsed_page) 
+  houses_strings = [] parsed_page.css('div.one-featured-prop').each do |home| 
+    houses_strings << home.content 
+  end 
+  houses = [] houses_strings.each do |house| 
+    new_house = house.split 
+    houses << new_house 
+  end 
+  byebug 
 end
-scraper
+# def scraper
+#   url = 'https://meqasa.com/'
+#   unparsed_page = HTTParty.get(url)
+#   parsed_page = Nokogiri::HTML(unparsed_page)
+#   houses = []
+#   first_house = parsed_page.css('div.one-featured-prop') # 121 houses
+#   first_house.each do |_homes|
+#     house = {
+#       Status: first_house.css('label').text,
+#       Information: first_house.css('p').text,
+#     }
+#     houses << house
+#   end
+#   byebug
+# end
+# scraper
