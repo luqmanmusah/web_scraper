dd6da9ab5c3d46d345590476ba69cb6f2bd33660
luqmanmusah <musahluqman@gmail.com>
2021-03-19 17:17:17 +0000

    Fix linter error

--- lib/web_scraper.rb
+++ lib/web_scraper.rb
@@ -3,13 +3,6 @@ require 'httparty'
 require 'byebug'
 require 'csv'
 
-public
-def parsed_page
-  url = 'https://meqasa.com/'
-  unparsed_page = HTTParty.get(url)
-  Nokogiri::HTML(unparsed_page)
-end
-
 def featured
   parsed = parsed_page
   featured_houses = []
@@ -25,28 +18,3 @@ def featured
   houses
 end
 
-def scraper
-  houses = featured
-  h = %w[status description bed washroom garage location]
-  CSV.open('data.csv', 'w', write_headers: true, headers: h) do |csv|
-    houses.each do |house|
-      house[0] = house[0..1].join(' ')
-      house[1] = house[2..9].join(' ').split('at')[0]
-      house[5] = house[2..9].join(' ').split('at')[1]
-      if house[house.length - 3].scan(/\D/).empty?
-        house[2] = house[house.length - 3]
-        house[3] = house[house.length - 2]
-        house[4] = house[house.length - 1]
-
-      else
-        house[2] = house[house.length - 2]
-        house[3] = house[house.length - 1]
-        house[4] = '0'
-      end
-      house.slice!(6..house.length - 1)
-      csv << house
-    end
-  end
-  # byebug
-end
-scraper
