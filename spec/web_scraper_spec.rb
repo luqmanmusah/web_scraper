c3ffd580f34b2aa264c7cb12e7ff0b8da69093e4
luqmanmusah <musahluqman@gmail.com>
2021-03-19 20:04:23 +0000

    Modified web_scraper file

--- spec/web_scraper_spec.rb
+++ spec/web_scraper_spec.rb
@@ -1,13 +1,17 @@
 require '../lib/web_scraper'
 
+
 describe '#featured' do
   it 'return an array' do
     expect(featured.class).to eq(Array)
   end
+end
+
+describe '#scraper' do
   it 'shoud return an Array of the houses' do
-    expect(featured.class).to eql(Array)
+    expect(scraper.class).to eql(Array)
   end
   it 'should return the length of the houses' do
-    expect(featured.length > 100).to eql true
+    expect(scraper.length > 50).to eql true
   end
 end
