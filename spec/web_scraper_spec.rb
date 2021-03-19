4f027304bbef7445fce3f4493ae7e0c9d3b41f8d
luqmanmusah <musahluqman@gmail.com>
2021-03-19 16:54:32 +0000

    Add scraper method to executable file

--- spec/web_scraper_spec.rb
+++ spec/web_scraper_spec.rb
@@ -1,25 +1,13 @@
-require '../bin/web_scraper'
-
-# describe '#parsed_page' do
-# let(:url) {'https://meqasa.com/'}
-# let(:unparsed_page) {HTTParty.get(url)}
-# let(:parsed) {Nokogiri::HTML(unparsed_page)}
-#     it 'return an object of type nokogiri' do
-#     expect(parsed_page).to eql(parsed)
-#     end
-# end
+require '../lib/web_scraper'
 
 describe '#featured' do
   it 'return an array' do
     expect(featured.class).to eq(Array)
   end
-end
-
-describe '#scraper' do
   it 'shoud return an Array of the houses' do
-    expect(scraper.class).to eql(Array)
+    expect(featured.class).to eql(Array)
   end
   it 'should return the length of the houses' do
-    expect(scraper.length > 50).to eql true
+    expect(featured.length > 100).to eql true
   end
 end
